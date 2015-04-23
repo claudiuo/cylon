17cb90152640dfba713fcd04b2cf46cfe0bc3033
claudiuo <claudiuo@gmail.com>
2015-04-23 22:15:14 +0000

    first commit

---
+++ mqtt-client.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var mqtt    = require('mqtt');
+var client  = mqtt.connect('mqtt://test.mosquitto.org');
+
+client.on('message', function (topic, payload) {
+  // MQTT only receives and sends message payload as
+  // string or buffer, so we need to parse the JSON string
+  // send by Cylon, this way we can access it as a regular
+  // JS object.
+  var data, sender;
+
+  if (!!payload && (payload.length > 0)) {
+    data = JSON.parse(payload);
+    sender = data.sender;
+  }
+
+  if (sender !== 'self') {
+    console.log('Topic name ==>', topic);
+    console.log('Payload ==>', data);
+  }
+});
+
+// Payload needs to be a JSON string
+var payload = JSON.stringify({
+  sender: 'self'
+});
+
+client.subscribe('/api/robots');
+client.publish('/api/robots', payload);
+
+client.subscribe('/api/robots/cybot');
+client.publish('/api/robots/cybot', payload);
+
+client.subscribe('/api/robots/cybot/devices');
+client.publish('/api/robots/cybot/devices', payload);
+
+client.subscribe('/api/robots/cybot/events');
+client.publish('/api/robots/cybot/events', payload);
+
+client.subscribe('/api/robots/cybot/commands');
+client.publish('/api/robots/cybot/commands', payload);
+
+// In order to be able to pass params to the commands
+// we need to convert them to json, since MQTT only
+// accepts strings and buffers as the paylod.
+// Cylon.js will convert the `args` array passed
+// in the payload and pass the params to the
+// command, method or function as regular params,
+// in the same way `function.apply()` would do.
+var params = {
+  sender: 'self',
+  args: ['param1', 'param2', 'param3']
+};
+
+payload = JSON.stringify(params);
+
+client.subscribe('/api/robots/cybot/commands/#');
+
+setInterval(function() {
+  client.publish(
+    '/api/robots/cybot/command1',
+    payload);
+}, 2000);
+
+//client.end();
---
+++ robot.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var Cylon = require('cylon');
+var toggleIndex = false;
+
+Cylon.robot({
+  name: 'cybot',
+
+  // These are the events that will be registered in the API
+  // TODO: figure out how to use events!!!
+  events: ['event1', 'event2', 'event3'],
+  
+  // These are the commands that will be available in the API
+  // Commands method needs to return an object with the aliases
+  // to the robot methods.
+  commands: function() {
+    return {
+      cmd1: this.command1,
+      cmd2: this.command2,
+      cmd3: this.command3
+    };
+  },
+
+  connections: {
+    loopback: { adaptor: 'loopback' }
+  },
+
+  devices: {
+    ping: { driver: 'ping' }
+  },
+
+  work: function() {
+    // for this example we will use API calls to command the robot
+  },
+
+  command1: function() {
+    console.log("command1");
+    this.emit('event1');
+  },
+
+  command2: function() {
+    console.log("command2");
+    this.emit('event2');
+  },
+
+  command3: function(data) {
+    console.log(data);
+    toggleIndex = !toggleIndex;
+    console.log("command3-"+toggleIndex);
+    this.emit('event3');
+    if (toggleIndex) {
+      this.emit('event1');
+    } else {
+      this.emit('event2');
+    }
+  }  
+});
+
+// ensure you install the API plugin first:
+// $ npm install cylon-api-mqtt
+Cylon.api('mqtt',{
+  broker: 'mqtt://test.mosquitto.org'//,
+  //port: '3000'
+});
+
+Cylon.start();
